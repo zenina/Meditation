3e3aac9672778419cc67816339766808f1c6dbfd
Nina <zenlimmer@gmail.com>
2018-02-05 00:21:10 -0700

    updated some affirmations, I am planning to split these off into their own file to be sourced

--- meditate.sh
+++ meditate.sh
@@ -3,15 +3,24 @@ Passions=(Anime, Art, Physics)
 Role="Sr. Linux Systems Engineer"
 
 
-
+# source logger function/file
+. ${PWD}/logger.inc
+cat ${PWD}/ascii-meditate 
 
 initDay(){
+cat ${PWD}/ascii-meditate 
+
+## Formatting 
 echo " (~.~) Meditate"
+
 echo "Play with puppy"
-echo "Be productive and thoughtful with how I use my time"
-echo "Stay focused on daily objectives"
+echo "Be productive and thoughtful with how I ration the use of time; both my time and that of others."
+echo "Eat healthy, exercise, and take vitamins."
+echo "Use every negative experience to your advantage."
+echo "Stay focused on daily objectives. Focus forward."
 echo "Get work done"
 echo "Be happy"
+echo " (~.~) Meditate"
 echo "Be respectful to others"
 echo "Work towards improving and valuing your reputation"
 echo "Respect friendships, relationships, family and colleagues"
@@ -19,21 +28,30 @@ echo " (~.~) Meditate"
 echo "Work towards long-term goals"
 echo "Have fun!"
 
+
 nix(){ 
 	echo "style of meditation"
 }
 
 	# 16 hours out of every day (when I'm not sleeping)
-	for ((i=0;i<16;i++)) ;
+#	for ((i=0;i<16;i++)) ;
+	for ((i=0;i<2;i++)) ;
 	do
 	set -x
 	cat <<EOF
 	$(eval nix)
 	logger pink "Find time for: ${Passions[*]}"
 EOF
-	sleep 3600
+	sleep 3
+
+	echo "Chamomile tea"
+	echo "Get plenty of rest"
+	echo "Good night!"
+	echo " (~.~) Meditate"
+	cat ${PWD}/ascii-meditate 
 	done
-sleep 480
+sleep 3
+#sleep 480
 }
 
 initDay
